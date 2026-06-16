33b8bd6f1738af4acc553ef6161521fc77208c64
Guillermo Julián <guillermo.julian@datadoghq.com>
2026-06-16 14:03:43 +0000

    Add kueue_workload_uid tag at orchestrator cardinality

--- comp/core/tagger/collectors/workloadmeta_test.go
+++ comp/core/tagger/collectors/workloadmeta_test.go
@@ -1711,6 +1711,7 @@ func TestHandleKubeKueueWorkload(t *testing.T) {
 			EntityMeta: workloadmeta.EntityMeta{
 				Name:      "job-sample",
 				Namespace: "default",
+				UID:       "uid-job-sample",
 			},
 			QueueName:        "batch",
 			ClusterQueueName: "cluster-batch",
@@ -1720,15 +1721,17 @@ func TestHandleKubeKueueWorkload(t *testing.T) {
 
 	expected := []*types.TagInfo{
 		{
-			Source:               kueueWorkloadSource,
-			EntityID:             types.NewEntityID(types.KueueWorkload, workloadID.ID),
-			IsComplete:           true,
-			HighCardTags:         []string{},
-			OrchestratorCardTags: []string{},
+			Source:       kueueWorkloadSource,
+			EntityID:     types.NewEntityID(types.KueueWorkload, workloadID.ID),
+			IsComplete:   true,
+			HighCardTags: []string{},
+			OrchestratorCardTags: []string{
+				"kueue_workload:job-sample",
+				"kueue_workload_uid:uid-job-sample",
+			},
 			LowCardTags: []string{
 				"kueue_cluster_queue:cluster-batch",
 				"kueue_local_queue:batch",
-				"kueue_workload:job-sample",
 			},
 			StandardTags: []string{},
 		},
@@ -1846,6 +1849,8 @@ func TestKueueWorkloadResourceFlavorTagsPropagateToPodContainers(t *testing.T) {
 					"kueue_cluster_queue:cluster-batch",
 					"kueue_local_queue:batch",
 					"kueue_resource_flavor:a100",
+				})
+				assert.Subset(t, tagInfo.OrchestratorCardTags, []string{
 					"kueue_workload:job-sample",
 				})
 				return
